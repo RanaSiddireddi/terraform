6560da8df8465b62c6bbfe8f060566f1c19c18dc
Rana <rana.kaushik@local.com>
2023-04-07 21:40:38 +1000

    WIP

--- backend.tf
+++ backend.tf
@@ -5,15 +5,3 @@ terraform {
     region = "us-east-1"
   }
 }
-
-
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.62.0"
-    }
-  }
-}
-
-provider "aws" {}
